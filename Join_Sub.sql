e2972107594f08e0dae06af1be32ade24e88391f
pdu08075 <pdu08075>
2024-07-25 12:01:44 +0900

    2024.07.25.11

--- Join_Sub.sql
+++ Join_Sub.sql
@@ -55,12 +55,144 @@ FROM department dpt;
 -- INNER JOIN: 두 테이블에서 조건이 일치하는 레코드만 반환
 -- SELECT column, ... FROM 기준 테이블 INNER JOIN 조합할 테이블 ON 조인 조건
 
-SELECT *
+SELECT
+	E.employee_number '사번',
+    E.name '사원이름',
+    E.age '나이',
+    D.department_code '부서코드',
+    D.name '부서명',
+    D.tel_number '부서전화번호'
 FROM employee E INNER JOIN department D
 ON E.department_code = D.department_code;
 
+-- LEFT OUTER JOIN (LEFT JOIN): 기준 테이블의 모든 레코드와 조합할 테이블 중 일치하는 레코드만 반환
+-- 만약에 조합할 테이블에 조건에 일치하는 레코드가 존재하지 않으면 null로 표현
 
+SELECT
+	E.employee_number '사번',
+    E.name '사원이름',
+    E.age '나이',
+    E.department_code '부서코드',
+    D.name '부서명',
+    D.tel_number '부서전화번호'
+FROM employee E LEFT OUTER JOIN department D
+ON E.department_code = D.department_code;
 
+-- RIGHT OUTER JOIN (RIGHT JOIN) : 조합할 테이블의 모든 레코드와 기준 테이블 중 조건에 일치라는 레코드만 반환
+-- 만약 기준 테이블에 조건에 일치하는 레코드가 존재하지 않으면 null로 반환
+
+SELECT
+	E.employee_number '사번',
+    E.name '사원이름',
+    E.age '나이',
+    D.department_code '부서코드',
+    D.name '부서명',
+    D.tel_number '부서전화번호'
+FROM employee E RIGHT JOIN department D
+ON E.department_code = D.department_code;
+
+-- FULL OUTER JOIN (FULL JOIN): 기준 테이블의 모든 레코드와 조합할 테이블의 모든 레코드를 반환
+-- 만약 기준 테이블 혹은 조합할 테이블에 조건에 일치하는 레코드가 존재하지 않으면 null로 반환
+-- MySQL에서는 FULL OUTER JOIN을 문법상 제공하지 않음
+-- FULL JOIN = LEFT JOIN + RIGHT JOIN
+
+SELECT
+	E.employee_number '사번',
+    E.name '사원이름',
+    E.age '나이',
+    E.department_code '부서코드',
+    D.name '부서명',
+    D.tel_number '부서전화번호'
+FROM employee E LEFT OUTER JOIN department D
+ON E.department_code = D.department_code
+UNION
+SELECT
+	E.employee_number '사번',
+    E.name '사원이름',
+    E.age '나이',
+    D.department_code '부서코드',
+    D.name '부서명',
+    D.tel_number '부서전화번호'
+FROM employee E RIGHT JOIN department D
+ON E.department_code = D.department_code;
+
+-- CROSS JOIN: 기준 테이블의 각 레코드를 조합할 테이블의 모든 레코드에 조합하여 반환
+-- CROSS JOIN 결과 레코드 수 = 기준 테이블 레코드
+SELECT *
+FROM employee E CROSS JOIN department D;
+-- MySQL에서 기본 조인이 CROSS JOIN 형태임
+SELECT *
+FROM employee E JOIN department D;
+
+SELECT *
+FROM employee E, department D;
+
+-- 부서코드가 A인 사원에 대해
+-- 사번, 이름, 부서명을 조회하시오.
+SELECT
+	E.employee_number '사번',
+    E.name '이름',
+    D.name '부서명'
+FROM employee E INNER JOIN department D
+ON E.department_code = D.department_code
+WHERE E.department_code = 'A';
+
+-- 부서명이 '영업부'인 사원에 대해
+-- 사번, 이름, 나이를 조회하시오
+SELECT 
+	E.employee_number '사번',
+    E.name '이름',
+    E.age '나이'
+FROM employee E INNER JOIN department D
+ON E.department_code = D.department_code
+WHERE D.name = '영업부';
+
+-- 쿼리 내부에 존재하는 또 다른 쿼리, 쿼리 결과를 조건이나테이블로 사용할 수 있도록 함
+
+-- WHERE 절에서 서브쿼리: 조회 결과를 조건으로 사용하여 조건을 동적으로 지정할 수 있도록 함
+-- WHERE 절에서 비교 연산 등으로 사용할 때 조회하는 컬럼의 개수 및 레코드의 개수 주의
+SELECT employee_number, name, age
+FROM employee
+WHERE department_code = (
+	SELECT department_code
+    FROM department
+    WHERE name = '영업부'
+);
+
+SELECT employee_number, name, age
+FROM employee
+WHERE department_code IN (
+	SELECT department_code
+    FROM department
+    -- WHERE name = '영업부'
+);
+
+SELECT employee_number, name, age
+FROM employee
+WHERE department_code = (
+	SELECT department_code
+	FROM department
+	WHERE name = '영업부'
+);
+
+-- FROM 절에서 서브쿼리: 조회 결과 테이블을 다시 FROM 절에서 재사용
+SELECT *
+FROM
+
+(SELECT 
+	E.employee_number '사번',
+    E.name '이름',
+    E.age '나이'
+FROM employee E INNER JOIN department D
+WHERE D.name = '영업부')
+ON E.department_code = D.department_code;
+
+-- 서브쿼리를 FROM 절에서 사용할 땐 반드시 별칭을 사용해야 함
+SELECT *
+FROM(
+	SELECT * FROM department
+) D
+WHERE name = '영업부';
 
 
 
